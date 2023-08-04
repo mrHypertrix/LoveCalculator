9e6d879975e6740de1e38de47db26128683c748b
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-05 00:23:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import telebot
-import axios
-import telebot.types
+import requests
 
 BASE_URL = "https://love-calculator.p.rapidapi.com"
 
@@ -8,13 +7,17 @@ BOT_TOKEN = "5868706821:AAFG_JUGNaB9RsEDvOet22EQsLotaKDrM-U"
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.register_message_handler(telebot.message.Command("LOVE_CALCULATE"))
-def love_calculate(message: telebot.types.Message):
-    your_name = message.text
+@bot.message_handler(commands=['love'])
+def love_calculate(message):
+    your_name = message.text.split()[1]  # Extract the name from the command
     partner_name = "ashi"
 
-    response = axios.get(
-        f"{BASE_URL}/getPercentage?yourName={your_name}&partnerName={partner_name}"
+    response = requests.get(
+        f"{BASE_URL}/getPercentage?yourName={your_name}&partnerName={partner_name}",
+        headers={
+            "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+            "x-rapidapi-key": "b5f715ba64msha7ad36c828d9b16p14c38fjsnf07017e60f2a"
+        }
     )
 
     if response.status_code == 200:
