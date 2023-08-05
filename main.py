affb808c9e8b944071f2354a403988542e5bcd7f
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-05 09:15:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
+# Your main script
+
 import telebot
 import config
 
 def calculate_love_percentage(your_name, partner_name):
-  """Calculates the love percentage between two names.
+    """Calculates the love percentage between two names.
 
-  Args:
-    your_name: The name of the first person.
-    partner_name: The name of the second person.
+    Args:
+        your_name: The name of the first person.
+        partner_name: The name of the second person.
 
-  Returns:
-    The love percentage between the two names.
-  """
+    Returns:
+        The love percentage between the two names.
+    """
+    your_name_score = sum(ord(letter) - ord('A') + 1 for letter in your_name.upper())
+    partner_name_score = sum(ord(letter) - ord('A') + 1 for letter in partner_name.upper())
+    love_percentage = (your_name_score * partner_name_score) % 100
+    return love_percentage
 
-  your_name_score = sum(ord(letter) - ord('A') + 1 for letter in your_name)
-  partner_name_score = sum(ord(letter) - ord('A') + 1 for letter in partner_name)
-  love_percentage = (your_name_score * partner_name_score) % 100
-  return love_percentage
-
-def start(message):
-  bot.send_message(message.chat.id, 'Welcome to the Love Calculator!')
-
-def love(message):
-  your_name = message.text.split(' ')[1]
-  partner_name = message.text.split(' ')[2]
-  love_percentage = calculate_love_percentage(your_name, partner_name)
-  bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
-  bot.send_message(message.chat.id, f'❤️❤️❤️')
-
-bot = telebot.TeleBot(api_id=str(config.api_id), api_hash=config.api_hash)
+bot = telebot.TeleBot(config.bot_token)
 
 @bot.message_handler(commands=["start"])
-def start(message):
-  bot.send_message(message.chat.id, 'Welcome to the Love Calculator!')
+def start_message(message):
+    bot.send_message(message.chat.id, 'Welcome to the Love Calculator!')
 
 @bot.message_handler(commands=["love"])
-def love(message):
-  your_name = message.text.split(' ')[1]
-  partner_name = message.text.split(' ')[2]
-  love_percentage = calculate_love_percentage(your_name, partner_name)
-  bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
-  bot.send_message(message.chat.id, f'❤️❤️❤️')
+def love_message(message):
+    names = message.text.split(' ')[1:]
+    if len(names) >= 2:
+        your_name = names[0]
+        partner_name = names[1]
+        love_percentage = calculate_love_percentage(your_name, partner_name)
+        bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
+        bot.send_message(message.chat.id, '❤️❤️❤️')
+    else:
+        bot.send_message(message.chat.id, 'Please provide two names for the love calculation.')
 
 bot.polling()
