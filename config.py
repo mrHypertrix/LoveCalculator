165ed484d68eb527eed6db36b42c0584e1c96956
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-05 09:03:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = 29131399
 api_hash = bf43dced8b0dbe587788e3a45d89f1f7
-bot_token = 5868706821:AAFG_JUGNaB9RsEDvOet22EQsLotaKDrM-U
+bot_token = '5868706821:AAFG_JUGNaB9RsEDvOet22EQsLotaKDrM-U'
