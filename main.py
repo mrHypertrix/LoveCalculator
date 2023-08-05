c815b47baa48af51cb01f29dc50d97e1785f9e39
Quantomize <131014805+mrHypertrix@users.noreply.github.com>
2023-08-05 08:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,18 @@ def calculate_love_percentage(your_name, partner_name):
   love_percentage = (your_name_score * partner_name_score) % 100
   return love_percentage
 
-bot = telebot.TeleBot('5868706821:AAFG_JUGNaB9RsEDvOet22EQsLotaKDrM-U')
+bot = telebot.TeleBot('YOUR_BOT_TOKEN')
 
-@bot.on('message')
-def handle_message(message):
-  if message.chat.type == 'group' and message.chat.title == 'Work Group':
-    if message.text == '/start':
-      bot.send_message(message.chat.id, 'Welcome to the Love Calculator!')
-    elif message.text == 'your name':
-      bot.send_message(message.chat.id, 'Enter your name:')
-    elif message.text == 'partner\'s name':
-      your_name = message.chat.first_name
-      partner_name = message.text
-      love_percentage = calculate_love_percentage(your_name, partner_name)
-      bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
-    elif message.text == '/love':
-      your_name = message.text.split(' ')[1]
-      partner_name = message.text.split(' ')[2]
-      love_percentage = calculate_love_percentage(your_name, partner_name)
-      bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
-      bot.send_message(message.chat.id, f'❤️❤️❤️')
-  else:
-    bot.send_message(message.chat.id, 'This bot is only available to work groups.')
+@bot.message_handler(commands=["start"])
+def start(message):
+  bot.send_message(message.chat.id, 'Welcome to the Love Calculator!')
+
+@bot.message_handler(commands=["love"])
+def love(message):
+  your_name = message.text.split(' ')[1]
+  partner_name = message.text.split(' ')[2]
+  love_percentage = calculate_love_percentage(your_name, partner_name)
+  bot.send_message(message.chat.id, f'Your love percentage is {love_percentage}%')
+  bot.send_message(message.chat.id, f'❤️❤️❤️')
 
 bot.polling()
